4e5883ef40e66023d48653609e0cbe9c919719f5
prajaktm03 <prajakt_madkar@persistent.com>
2024-10-27 00:12:29 +0530

    Update code

--- App/app.js
+++ App/app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World --- Shubh Deepavali \n');
+  res.end('Hello World -- back to original \n');
 });
 
 server.listen(port, hostname, () => {
